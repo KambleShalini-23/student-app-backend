9e5dd2ef9cd078788c12484a85e11c2aa13e0bc2
KambleShalini-23 <shalinikamble2399@gmail.com>
2025-11-10 23:57:27 -0500

    Initial commit with proper .gitignore

---
+++ src/main/java/com/studentapp/student_app_backend/StudentAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.studentapp.student_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentapp/student_app_backend/StudentAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentapp.student_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
